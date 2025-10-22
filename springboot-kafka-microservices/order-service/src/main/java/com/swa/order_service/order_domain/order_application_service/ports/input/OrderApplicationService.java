1d8d45608e9feb5adb08fc0e9528f093a04dd548
TrongHiuuu <nthieu.c4pbchau2122@gmail.com>
2025-10-23 02:38:12 +0700

    23/10/2025 - Update Delete
    + Thêm phương thức trừu tượng updateDeleteItems

--- springboot-kafka-microservices/order-service/src/main/java/com/swa/order_service/order_domain/order_application_service/ports/input/OrderApplicationService.java
+++ springboot-kafka-microservices/order-service/src/main/java/com/swa/order_service/order_domain/order_application_service/ports/input/OrderApplicationService.java
@@ -1,6 +1,5 @@
 package com.swa.order_service.order_domain.order_application_service.ports.input;
 
-import com.swa.order_service.order_domain.order_application_service.dto.Item;
 import com.swa.order_service.order_domain.order_application_service.dto.create.CreateOrderCommand;
 import com.swa.order_service.order_domain.order_application_service.dto.create.CreateOrderResponse;
 import com.swa.order_service.order_domain.order_application_service.dto.history.HistoryOrderResponse;
@@ -9,6 +8,8 @@ import com.swa.order_service.order_domain.order_application_service.dto.rating.R
 import com.swa.order_service.order_domain.order_application_service.dto.statistics.StatisticsResponse;
 import com.swa.order_service.order_domain.order_application_service.dto.update.add.UpdateAddItemsCommand;
 import com.swa.order_service.order_domain.order_application_service.dto.update.add.UpdateAddItemsResponse;
+import com.swa.order_service.order_domain.order_application_service.dto.update.delete.UpdateDelItemsCommand;
+import com.swa.order_service.order_domain.order_application_service.dto.update.delete.UpdateDelItemsResponse;
 
 import java.util.List;
 import java.util.UUID;
@@ -24,6 +25,7 @@ public interface OrderApplicationService {
     RateOrderResponse rateOrder(RateOrderCommand command);
 
     UpdateAddItemsResponse updateAddItems(UpdateAddItemsCommand command);
+    UpdateDelItemsResponse updateDeleteItems(UpdateDelItemsCommand command);
 //    //UseCase:Trackorder
 //    TrackOrderResponsetrackOrder(TrackOrderQueryquery);
 //    //UseCase:Cancelorder
