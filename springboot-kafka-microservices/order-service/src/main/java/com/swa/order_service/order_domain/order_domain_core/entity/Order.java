5461dbb732a539f706ad7105f0ac603211ec39ef
TrongHiuuu <nthieu.c4pbchau2122@gmail.com>
2025-10-23 01:22:07 +0700

    23/10/2025 - Update Add
    + Thêm phương thức validateAndUpdateAddItems cho xử lý use case update add, đồng thời thêm các method phụ:
       - checkIfOrderStatusIsPending
       - addItemsAndCalculatePrice: thực hiện thêm item và tính toán lại totalPrice
       - helper method calculateTotalPrice

--- springboot-kafka-microservices/order-service/src/main/java/com/swa/order_service/order_domain/order_domain_core/entity/Order.java
+++ springboot-kafka-microservices/order-service/src/main/java/com/swa/order_service/order_domain/order_domain_core/entity/Order.java
@@ -7,14 +7,14 @@ import com.swa.order_service.order_domain.order_domain_core.valueobject.OrderSta
 import com.swa.order_service.order_domain.order_domain_core.valueobject.OrderRating;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Getter
-@Builder
+@Builder(toBuilder = true)
 public class Order {
     // Immutable fields - set once in the constructor
     private final UUID customerId;
@@ -108,6 +108,72 @@ public class Order {
         validateRating(orderRating);
         rating = orderRating;
     }
+
+    public void checkIfOrderStatusIsPending() {
+        if (this.orderStatus != OrderStatus.PENDING) {
+            throw new OrderDomainException("Order status is not valid (must be PENDING)");
+        }
+    }
+
+    public Order validateAndUpdateAddItems(List<OrderItem> items) {
+        // Check order status
+        checkIfOrderStatusIsPending();
+        for (OrderItem item: items) {
+            validateOrderItem(item);
+        }
+        Order updatedAddOrder = addItemsAndCalculatePrice(items);
+        validateTotalPrice();
+        //
+        return updatedAddOrder;
+    }
+
+    public Order addItemsAndCalculatePrice(List<OrderItem> newItems) {
+        // Tạo list item updated để update bằng cách copy list item của order
+        List<OrderItem> updated = new ArrayList<>(this.items);
+
+        // Với mỗi item trong list truyền vào
+        for (OrderItem item : newItems) {
+            // Đặt cờ hiệu
+            boolean found = false;
+            // Duyệt qua từng phần tử trong list updated
+            for (int i = 0; i < updated.size(); i++) {
+                // Lấy ra phần tử hiện tại của list updated trong vòng lặp gán vào biến existing
+                OrderItem existing = updated.get(i);
+                // Nếu id của existing trùng với id của item ở vòng for ngoài (item trong newItems)
+                if (existing.getProductId().equals(item.getProductId())) {
+                    // => đã tìm thấy có tồn tại,
+                    // tăng số lượng của item lên bật cờ hiệu và break ra khỏi vòng lặp
+                    updated.set(i, existing.increaseQuantity(item.getQuantity()));
+                    //, đồng thời tăng thêm price bằng subtotal,
+
+                    found = true;
+                    break;
+                }
+            }
+            // Nếu cờ hiệu không được bật (tức item hiện tại trong newItems không trùng với items nào trong updated)
+            if (!found) {
+                updated.add(item);
+            }
+        }
+
+        // Tính lại tổng tiền sau khi có được danh sách các items sau cập nhật
+        Money updatedPrice = calculateTotalPrice(updated);
+
+        // Trả về chính bản thân object Order với list items và total price sau khi cập nhật
+        return this.toBuilder()
+                .items(updated)
+                .price(updatedPrice)
+                .build();
+    }
+
+    // Helper method: Tính tổng tiền của danh sách item
+    private Money calculateTotalPrice(List<OrderItem> items) {
+        return items.stream()
+                .map(OrderItem::getSubTotal)
+                .reduce(Money.ZERO, Money::add);
+    }
+
+
     /**
      * BUSINESS LOGIC: Mark order as PAID
      */
