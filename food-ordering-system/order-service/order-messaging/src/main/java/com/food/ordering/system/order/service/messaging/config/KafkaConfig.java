e736b251ed1263032e27e3e4067ec5a0ce1b07f2
LE NGOC THAO VY <thaovy3724@gmail.com>
2025-10-25 21:50:46 +0700

    kafka

--- food-ordering-system/order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/config/KafkaConfig.java
+++ food-ordering-system/order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/config/KafkaConfig.java
@@ -1,33 +1,18 @@
 package com.food.ordering.system.order.service.messaging.config;
 
 import lombok.Value;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaConfig {
-    @Value("${spring.kafka.bootstrap-servers}") String bootstrapServers;
     @Bean
-    public ProducerFactory<String, MyMsg> producerFactory() {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
-        return new DefaultKafkaProducerFactory<>(config);
-    }
-    @Bean
-    public NewTopic taskTopic() {
-        return TopicBuilder.name("topic-name")
+    public NewTopic transactionTopic(){
+        return TopicBuilder.name("order-topic")
                 .partitions(1)
                 .replicas(1)
                 .build();
-    }
-    @Bean
-    public KafkaTemplate<String, MyMsg> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
     }
 }
